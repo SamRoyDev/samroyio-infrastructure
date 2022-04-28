96d1a9925234fc16c7f827733e17e50f3d582033
samroy92 <samroy92@live.com>
2022-04-27 20:59:34 -0700

    moved backend init to main

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "3.44.0"
     }
   }
-  required_version = "> 0.14"
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }

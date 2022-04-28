c5bfeefc081ae46ed8ff82307f132fac355f2bea
samroy92 <samroy92@live.com>
2022-04-27 21:48:52 -0700

    adding cname

--- cloudflare.tf
+++ cloudflare.tf
@@ -2,3 +2,10 @@ provider "cloudflare" {
     email = "$CLOUDFLARE_EMAIL"
     api_key = "$CLOUDFLARE_API_KEY"
 }
+
+resource "cloudflare_record" "www" {
+    name = "www"
+    type = "CNAME"
+    value = "x.com"
+    ttl = 3600
+}

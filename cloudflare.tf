5f2a3e13d647e2b4ea0e5d8a67466e6e1cdf8d18
samroy92 <samroy92@live.com>
2022-04-27 22:00:26 -0700

    fixing zone id location

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,11 +1,11 @@
 provider "cloudflare" {
     email = var.CLOUDFLARE_EMAIL
     api_key = var.CLOUDFLARE_API_KEY
-    zone_id = var.CLOUDFLARE_ZONE_ID
 }
 
 resource "cloudflare_record" "www" {
     name = "www"
+    zone_id = var.CLOUDFLARE_ZONE_ID
     type = "CNAME"
     value = "x.com"
     ttl = 3600

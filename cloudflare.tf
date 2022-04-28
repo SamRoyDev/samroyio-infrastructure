1ed54054da8d491e21f38bc3c0d8889c0a184166
samroy92 <samroy92@live.com>
2022-04-27 23:35:39 -0700

    updated with value

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,11 +1,11 @@
 provider "cloudflare" {
-    email = data.aws_ssm_parameter.CLOUDFLARE_EMAIL
-    api_key = data.aws_ssm_parameter.CLOUDFLARE_API_KEY
+    email = data.aws_ssm_parameter.CLOUDFLARE_EMAIL.value
+    api_key = data.aws_ssm_parameter.CLOUDFLARE_API_KEY.value
 }
 
 resource "cloudflare_record" "www" {
     name = "www"
-    zone_id = data.aws_ssm_parameter.CLOUDFLARE_ZONE_ID
+    zone_id = data.aws_ssm_parameter.CLOUDFLARE_ZONE_ID.value
     type = "CNAME"
     value = "x.com"
     ttl = 3600

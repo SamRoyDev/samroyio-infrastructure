63f6ea2cfcd4a2c38121e3b66fd94ef0e97dc7a9
samroy92 <samroy92@live.com>
2022-04-27 21:16:45 -0700

    added website s3 bucket

--- main.tf
+++ main.tf
@@ -10,3 +10,20 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+
+resource "aws_s3_bucket" "website-bucket" {
+    bucket = var.website-bucket
+    force_destroy = true
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
